72db53f100985c5e0cbe8a58f3c8cd55fd72e466
Mateus Garcia <mateusgarcia92@gmail.com>
2022-01-22 17:35:07 +0000

    Added game() function which plays a best of 3 game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,7 @@
 
-// initialises an array with the choices for the game: rock, paper, scissors
+// initialises an array with the choices for the game: rock, paper, scissors, and player selection as an empty string
 let choices = ["rock", "paper", "scissors"];
+let playerSelection = "";
 
 // function which determines the computer's choice by taking the array 'choices' as a parameter,
 // then initialising an index which will randomly select one of the choices and return the choice corresponding to the index number
@@ -9,47 +10,71 @@ function computerPlay (choices) {
     return choices[index];
 }
 
-//
-function gameRound (playerSelection, computerSelection) {
+// function which plays a round of rock, paper, scissors. it prompts the user to select their move,
+// initialises the return and then uses a switch  method to store the result in the result variable, returning the return at the end.
+function playRound (playerSelection, computerSelection) {
     
     playerSelection = window.prompt("Rock, paper, or scissors?");
     let result = "";
     
     switch (true) {
         case playerSelection.toLowerCase() == "rock" && computerSelection == "rock":
-            result = "Computer chose rock. \n Tie! Both players chose rock.";
+            result = "You chose rock. \nComputer chose rock. \n Tie! Both players chose rock.";
             break;
         case playerSelection.toLowerCase() == "paper" && computerSelection == "paper":
-            result = "Computer chose paper. \n Tie! Both players chose paper.";
+            result = "You chose paper. \nComputer chose paper. \n Tie! Both players chose paper.";
             break;
         case playerSelection.toLowerCase() == "scissors" && computerSelection == "scissors":
-            result = "Computer chose scissors. \n Tie! Both players chose scissors.";
+            result = "You chose scissors. \nComputer chose scissors. \n Tie! Both players chose scissors.";
             break;
         case playerSelection.toLowerCase() == "rock" && computerSelection == "paper":
-            result = "Computer chose paper. \n You lost! Paper beats rock.";
+            result = "You chose rock. \nComputer chose paper. \n You lost! Paper beats rock.";
             break;
         case playerSelection.toLowerCase() == "rock" && computerSelection == "scissors":
-            result = "Computer chose scissors. \n You win! Rock beats scissors.";
+            result = "You chose rock. \nComputer chose scissors. \n You win! Rock beats scissors.";
             break;
         case playerSelection.toLowerCase() == "paper" && computerSelection == "rock":
-            result = "Computer chose rock. \n You win! Paper beats rock.";
+            result = "You chose paper. \nComputer chose rock. \n You win! Paper beats rock.";
             break;
         case playerSelection.toLowerCase() == "paper" && computerSelection == "scissors":
-            result = "Computer chose scissors. \n You lost! Scissors beats paper.";
+            result = "You chose paper. \nComputer chose scissors. \n You lost! Scissors beats paper.";
             break;
         case playerSelection.toLowerCase() == "scissors" && computerSelection == "paper":
-            result = "Computer chose paper. \n You win! Scissors beats paper.";
+            result = "You chose scissors. \nComputer chose paper. \n You win! Scissors beats paper.";
             break;
         case playerSelection.toLowerCase() == "scissors" && computerSelection == "rock":
-            result = "Computer chose rock. \n You lost! Rock beats scissors.";    
+            result = "You chose scissors. \nComputer chose rock. \n You lost! Rock beats scissors.";    
             break;   
         default:
-            result = "Rock, paper, or scissors?"; 
+            result = "You must choose either rock, paper or scissors. Check your spelling :)"; 
             
     }
 
     return result;
 }
 
-let playerSelection = "";
-let computerSelection = computerPlay(choices);
+// function that plays a best of 5 game with a while loop that runs until either player
+// or computer have a score of 3, checking the result and adding 
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    while (playerScore < 3 && computerScore < 3) {
+        let computerSelection = computerPlay(choices);
+        let result = playRound(playerSelection, computerSelection);
+        console.log(result);
+        let winner = result.includes("You win");
+        let loser = result.includes("You lost");
+        if (winner) {
+            playerScore++;
+        } else if (loser) {
+            computerScore++;
+        }
+        console.log(`Score: \n You: ${playerScore}, Computer: ${computerScore}`);
+    }
+    if (playerScore == 3) {
+        console.log("You are the winner, congratulations!");
+    } else if (computerScore == 3) {
+        console.log("You lost. Better luck next time!")
+    }
+}
+
